4561748140425497bdfd7fbd6b2bc2a9f93d4514
Salman Tariq <salmantariq8018@gmail.com>
2026-01-29 18:45:57 +0500

    feat(navbar): make navbar responsive for mobile and tablet with toggle sidebar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,49 @@
+import { useState } from 'react';
 import logo from '../assets/logo.png';
 import menuIcon from '../assets/menu-icon.png'
+import { IoMdClose } from "react-icons/io";
+
 
 const Navbar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
   return (
     <nav className='px-4 sm:px-12 lg:px-24 xl:px-40 py-2 flex justify-between items-center text-white fixed w-full top-0 z-20'>
-      <img src={logo} className='w-32 sm:w-40' alt="" />
-      <ul className='hidden md:flex items-center gap-7 text-sm transition-all'>
-        <li><a href="#" className='sm:hover:border-b'>Home</a></li>
-        <li><a href="#program" className='sm:hover:border-b'>Program</a></li>
-        <li><a href="#about-us" className='sm:hover:border-b'>About us</a></li>
-        <li><a href="#campus" className='sm:hover:border-b'>Campus</a></li>
-        <li><a href="#testimonial" className='sm:hover:border-b'>Testimonial</a></li>
-        <li>
-          <button className='bg-white text-[#212121] px-6 py-2.5 rounded-4xl cursor-pointer hover:scale-105 transition-all'>
-            Contact us
-          </button>
-        </li>
-      </ul>
+      <img src={logo} className='w-32 sm:w-40' alt="nav-logo" />
+
+      {/* Mobile Sidebar */}
+      <div className={`relative text-white sm:text-sm
+         ${!isMenuOpen ? 'max-md:w-0 overflow-hidden' : 'max-md:w-60 max-md:pl-10'}
+         max-md:fixed top-0 right-0 bottom-0 max-md:min-h-screen max-md:h-full max-md:flex-col
+         max-md:bg-blue-600 max-md:text-white max-md:pt-20 flex lg:items-center transition-all
+         `}>
+
+        <IoMdClose className='text-3xl absolute right-4 top-4 lg:hidden cursor-pointer' onClick={()=>setIsMenuOpen(false)} />
+
+        <ul className='flex flex-col gap-5 md:flex md:flex-row md:items-center md:gap-7 transition-all'>
+          <li><a href="#" className='md:hover:border-b' onClick={()=>setIsMenuOpen(false)}>Home</a></li>
+          <li><a href="#program" className='md:hover:border-b' onClick={()=>setIsMenuOpen(false)}>Program</a></li>
+          <li><a href="#about-us" className='md:hover:border-b' onClick={()=>setIsMenuOpen(false)}>About us</a></li>
+          <li><a href="#campus" className='md:hover:border-b' onClick={()=>setIsMenuOpen(false)}>Campus</a></li>
+          <li><a href="#testimonial" className='md:hover:border-b' onClick={()=>setIsMenuOpen(false)}>Testimonial</a></li>
+          <li>
+            <button className='bg-white text-[#212121] px-6 py-2.5 rounded-4xl md:mr-2 cursor-pointer hover:scale-105 transition-all'>
+              Contact us
+            </button>
+          </li>
+        </ul>
+      </div>
+      
 
       {/* Mobile Menu icon */}
-      <img src={menuIcon} className='w-7 md:hidden cursor-pointer' alt="" />
+      <img
+       src={menuIcon}
+       className='w-7 md:hidden cursor-pointer'
+       alt="menu"
+       onClick={()=>setIsMenuOpen(true)}
+      />
     </nav>
   )
 }
